469ab497a45954223ca3fb24fad75aca8afe7193
iamtanmoy2002 <btanmoy776@gmail.com>
2024-11-17 11:53:56 +0530

    Added layout, picture, icons, fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,28 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Next.js App",
+  description: "for nextjs learning",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-spaceGrotesk",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({
   children,
@@ -14,9 +36,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover: text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <header>
             <SignedOut>
               <SignInButton />
